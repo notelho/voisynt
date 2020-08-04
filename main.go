93766d6bc073259cd1370b03958e0fcb50d86496
Nathan Botelho <nimbotelho@gmail.com>
2020-08-04 01:35:01 -0300

    feat: create script structure

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	errors "github.com/enbot/voisynt/errors"
+	helper "github.com/enbot/voisynt/helper"
+	language "github.com/enbot/voisynt/language"
+)
+
 func main() {
 
-	// http.serve(  )
+	var args = helper.ArgumentsCreate()
+	var message = args.Message
+	var path = args.Path
+
+	fmt.Print("message: " + args.Message)
+	fmt.Print("path: " + args.Path)
+
+	if helper.PathExists(path) {
+
+		var name = language.FixName(message)
+
+		if !helper.PathExists(path + name) {
+			file = audio.fetch(message)
+			audio = audio.distortion(file)
+			helper.FileCreate(audio)
+		}
+
+		fmt.Print(path + name)
+
+	} else {
+		errors.ThrowExit(errors.ErrorPathNotFound)
+	}
 
 }
