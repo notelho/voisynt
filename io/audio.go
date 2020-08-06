f483b62ab1aee6a61536fe2ec1b3cc1d3598b25c
Nathan Botelho <nimbotelho@gmail.com>
2020-08-06 00:15:35 -0300

    feat: create audio name utils

--- io/audio.go
+++ io/audio.go
@@ -1,13 +1,13 @@
 package io
 
 import (
-	"io"
-	"os"
 	"regexp"
 	"strings"
 )
 
-func AudioName(name string) string {
+var extension string = ".mp3"
+
+func normalizeName(name string) string {
 	name = strings.ReplaceAll(name, " ", "_")
 	reg, _ := regexp.Compile("[^a-zA-Z0-9_]+")
 	name = reg.ReplaceAllString(name, "")
@@ -15,16 +15,18 @@ func AudioName(name string) string {
 	return name
 }
 
-func AudioPath(path string, file string) string {
-	return path + "\\" + file + ".mp3"
+func DefaultAudioName(name string) string {
+	return normalizeName(name) + extension
 }
 
-func AudioCreate(path string, name string, stream io.ReadCloser) *os.File {
-	audio := AudioPath(path, name)
-	dir, _ := os.Open(path)
-	file, _ := os.Create(audio)
-	_, _ = io.Copy(file, stream)
-	defer dir.Close()
-	defer stream.Close()
-	return file
+func DownloadAudioName(name string) string {
+	return "download@" + normalizeName(name) + extension
+}
+
+func DistortedAudioName(name string) string {
+	return "distorted@" + normalizeName(name) + extension
+}
+
+func RoboticAudioName(name string) string {
+	return "robotic@" + normalizeName(name) + extension
 }
